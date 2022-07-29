a0719516ff090aab5148b812a6855ba9b1818af9
CarvalhoVincent <carvalho.vincent@hotmail.fr>
2022-07-29 17:28:50 +0200

    gameboard layout responsive ok

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("test")
+const gameBoard = document.getElementById("gameBoard");
+const gameMenu = document.getElementById("gameMenu");
+
+
+
+
+function newGameCPU() {
+    gameBoard.style.display = "initial";
+    gameMenu.style.display = "none";
+}
+
+function newGamePlayer() {
+    gameBoard.style.display = "initial";
+    gameMenu.style.display = "none";
+}
+
+function restartGame() {
+    gameBoard.style.display = "none";
+    gameMenu.style.display = "initial";
+}
