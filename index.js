d80bf20bcabe17696ff16e7ab53da8a5afe35c90
CarvalhoVincent <carvalho.vincent@hotmail.fr>
2022-07-29 21:30:16 +0200

    start game + hover

--- index.js
+++ index.js
@@ -18,3 +18,30 @@ function restartGame() {
     gameBoard.style.display = "none";
     gameMenu.style.display = "initial";
 }
+
+function hitBox(box) {
+    var boxChoice = document.getElementById(box);
+    var img = document.createElement('img');
+    var turn = document.getElementById('turn');
+    var allBox = document.getElementsByClassName('box');
+
+    if (turn.getAttribute("data-value") === "X") {
+        img.src = "./assets/icon-x.svg";
+        boxChoice.appendChild(img);
+        boxChoice.classList.remove("hoverClassX")
+        turn.setAttribute("data-value", "O")
+        turn.src = "./assets/icon-O-turn.svg";
+    } else {
+        img.src = "./assets/icon-o.svg";
+        boxChoice.appendChild(img);
+        boxChoice.classList.remove("hoverClassO");
+        turn.setAttribute("data-value", "X")
+        turn.src = "./assets/icon-x-turn.svg";
+    }
+
+}
+
+
+// for (all of allBox.getAttribute("data-value")=== "X" || allBox.getAttribute("data-value")=== "O")  {
+//         all.classList.add("hoverClassX");
+//      };
